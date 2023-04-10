bb586979759dcc270f0c44cf34ad6491c2424da7
Xavier Samson <xaviersamson2015@gmail.com>
2023-04-09 20:27:42 -0400

    dev

--- LogIn.gd
+++ LogIn.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 var show_password = false
+const SQLITE = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
+var db
+var db_name "res://DataStore/TicketSystem"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	db = SQLITE.new()
+	db.path = db_name
 	$Password_LN.secret = show_password
 
 func _on_Sprite_pressed():
@@ -22,4 +27,4 @@ func LogIn():
 	var username_input = $Username_LN.text
 	var password_input = $Password_LN.text
 	
-	
+
