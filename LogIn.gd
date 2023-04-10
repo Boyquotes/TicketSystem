262a6c6b013ad17cd106afa99e01e2e1d97da2be
Xavier Samson <xaviersamson2015@gmail.com>
2023-04-09 23:47:30 -0400

    Changed DB system

--- LogIn.gd
+++ LogIn.gd
@@ -1,15 +1,44 @@
 extends Node2D
+var database = PostgreSQLClient.new()
+
+var user = "postgres"
+var password = "allomap1405"
+var host = "34.124.115.93"
+var port = "5432"
+var databaseConnection = "TicketSystem"
 
 var show_password = false
-const SQLITE = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
-var db
-var db_name "res://DataStore/TicketSystem"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	db = SQLITE.new()
-	db.path = db_name
-	$Password_LN.secret = show_password
+	database.connect("connection_etablished", self, "selectFromDB")
+	database.connect("connection_error", self, "error")
+	database.connect("connection_closed", self, "closedConnection")
+	
+	database.connect_to_host("postgresql://" + user + ":" + password + "@" + host + ":" + port + "/" + databaseConnection)
+	$Password_LN.secret = true
+
+func _process(delta):
+	database.poll()
+	pass
+
+func closedConnection():
+	print("database has closed")
+
+func selectFromDB():
+	print("running select query")
+	
+	var data = database.execute("""
+	BEGIN;
+	SELECT * FROM "TicketSystem".users
+	""")
+	
+	for d in data:
+		print(data.data_row)
+	database.close()
+	
+func _exit_tree():
+	database.close()
 
 func _on_Sprite_pressed():
 	if show_password == true:
@@ -26,5 +55,7 @@ func _on_Button_pressed():
 func LogIn():
 	var username_input = $Username_LN.text
 	var password_input = $Password_LN.text
+	var tablename = "users"
+		
 	
 
